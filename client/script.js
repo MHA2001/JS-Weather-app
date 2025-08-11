c2624f7788573fde9d2c2fd0d06456097ded7896
MHA2001 <mha.akhoudndi@gmail.com>
2025-08-11 23:30:18 +0330

    feat: render the current section of weather App

--- client/script.js
+++ client/script.js
@@ -20,5 +20,42 @@ function getWeather(lat, lon) {
 				exclude: 'minutely, alerts',
 			},
 		})
-		.then((res) => console.log(res.data));
+		.then((res) => {
+			renderWeather(res.data);
+		})
+		.catch((e) => {
+			console.log(e);
+			alert('Error getting weather. pleas try again');
+		});
 }
+
+function renderWeather({ current, daily, hourly }) {
+	document.body.classList.remove('blurred');
+	renderCurrentWeather(current);
+	renderHourlyWeather(hourly);
+	renderDailyWeather(daily);
+}
+
+function setValue(selector, value) {
+	document.querySelector(`[data-${selector}]`).textContent = value;
+}
+
+function getIconUrl(icon, { large = false } = {}) {
+	const size = large ? '@2x' : '';
+	return `http://openweathermap.org/img/wn/${icon}${size}.png`;
+}
+
+const currentIcon = document.querySelector('[data-current-icon]');
+function renderCurrentWeather(current) {
+	currentIcon.src = getIconUrl(current.icon, { large: true });
+	setValue('current-temp', current.currentTemp);
+	setValue('current-hight', current.hightTemp);
+	setValue('current-low', current.lowTemp);
+	setValue('current-fl-low', current.lowFeelsLike);
+	setValue('current-fl-hight', current.hightFeelsLike);
+	setValue('current-wind', current.windSpeed);
+	setValue('current-precip', current.precip);
+	setValue('current-description', current.description);
+}
+function renderDailyWeather(daily) {}
+function renderHourlyWeather(hourly) {}
